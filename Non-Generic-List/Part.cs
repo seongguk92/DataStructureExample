b2995a9e532626299edaf811fb614b6c536fc09c
seongguk <live8681@naver.com>
2021-08-15 18:50:11 +0900

    List add

---
+++ Non-Generic-List/Part.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Non_Generic_List
+{
+    public class Part : IEquatable<Part> 
+    {
+        public string PartName { get; set; }
+        public int PartId { get; set; }
+        public override string ToString()
+        {
+            return $"ID : {PartId} Name : {PartName}";
+        }
+        public override bool Equals(object obj)
+        {
+            if(obj is null)
+            {
+                return false;
+            }
+            Part objAsPart = obj as Part;
+            if(objAsPart is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Equals(objAsPart);
+            }
+        }
+        public override int GetHashCode()
+        {
+            return PartId;
+        }
+        public bool Equals([AllowNull] Part other)
+        {
+            if(other is null)
+            {
+                return false;
+            }
+            return (this.PartId.Equals(other.PartId));
+        }
+    }
+}
---
+++ Non-Generic-List/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Non_Generic_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Part> parts = new List<Part>();
+            parts.Add(new Part() { PartName = "A_Part", PartId = 1 });
+            parts.Add(new Part() { PartName = "B_Part", PartId = 2 });
+            parts.Add(new Part() { PartName = "C_Part", PartId = 3 });
+            parts.Add(new Part() { PartName = "D_Part", PartId = 4 });
+            parts.Add(new Part() { PartName = "E_Part", PartId = 5 });
+
+            foreach (Part part in parts)
+            {
+                Console.WriteLine(part);
+            }
+            Console.WriteLine();
+            parts.Contains(new Part { PartName = "", PartId = 6 });
+            parts.Insert(2, new Part() { PartName = "F_Part", PartId = 6 });
+
+            foreach (Part part in parts)
+            {
+                Console.WriteLine(part);
+            }
+            Console.WriteLine();
+            parts.Remove(new Part() { PartId = 3, PartName = "C_Part_1" });
+            foreach (Part part in parts)
+            {
+                Console.WriteLine(part);
+            }
+            Console.WriteLine();
+            parts.RemoveAt(3);
+            foreach (Part part in parts)
+            {
+                Console.WriteLine(part);
+            }
+        }
+    }
+}
